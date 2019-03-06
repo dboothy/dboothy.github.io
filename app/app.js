552f63c900331d4c0eddf9f43de5edb913cea495
David Booth-Metwally <davidbmetwally@gmail.com>
2019-03-05 18:40:35 -0800

    structure complete

--- app/app.js
+++ app/app.js
@@ -10,28 +10,52 @@ Cards
 Scrollspy
 */
 
+//jquery Materialize initialization
 
-//Materialize Tooltip
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.tooltipped');
-    var instances = M.Tooltip.init(elems);
-  });
-
-
-//Materialize Parallax
-
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.parallax');
-    var instances = M.Parallax.init(elems);
-  });
+$(document).ready(function() {
+    $('.tooltipped').tooltip();
+    $(".collapsible div").mouseover(function() { $(this).trigger('click'); });
+    $('.parallax').parallax();
+    $('.scrollspy').scrollSpy();
 
 
 
-//Materialize Srollspy
+});
 
   document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.scrollspy');
-    var instances = M.ScrollSpy.init(elems);
+
+
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems);
+  
+      
   });
 
+  // document.addEventListener('DOMContentLoaded', function() {
+  //   var elems = document.querySelectorAll('.tooltipped');
+  //   var instances = M.Tooltip.init(elems);
+  // });
 
+//Materialize Tooltip pure JS
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   var elems = document.querySelectorAll('.tooltipped');
+//   var instances = M.Tooltip.init(elems);
+// });
+
+
+//Materialize Parallax pure JS
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   var elems = document.querySelectorAll('.parallax');
+//   var instances = M.Parallax.init(elems);
+// });
+
+
+
+//Materialize Srollspy pure JS
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   var elems = document.querySelectorAll('.scrollspy');
+//   var instances = M.ScrollSpy.init(elems);
+// });
